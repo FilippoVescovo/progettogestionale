165b6133d8e94d77c97d97c395549ef1bb2fecbf
FilippoVescovo <86468120+FilippoVescovo@users.noreply.github.com>
2022-06-30 17:48:22 +0200

    model

---
+++ src/main/java/it/progettogestionale/web/model/LogFileApp.java
@@ -0,0 +1,370 @@
+package it.progettogestionale.web.model;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalDateTime;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class LogFileApp {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idLogApp;
+	@Column
+	private LocalDateTime data;
+	@Column
+	private Integer nodoConsole;
+	@Column
+	private Time launchingMeetingDataGatheringStarting;
+	@Column
+	private BigDecimal avgAnalysisTime;
+	@Column
+	private Date automationEnablingDate;
+	@Column
+	private boolean done;
+	@Column
+	private String nome_App, apmCode, insertedInCastProgram, stakeholderEngagement,
+	stakeholderBrief, onBoardingKitDelivery, primaRestitution, ownerOnboarding, ownerAFP, gdsUnit, tecnologia, serverManager,
+	soloCMS, macchina, noteOnboarding, fase, afpStatus, pubblicatoDashboard, noteAppOwner,  jiraautomationActivation,
+	repoAvailability, automationStatus, automationNotes, greenItIndex, onboardingKitClosing, sourceCodeFinalDelivery,
+	linkConfluence, businessCriticality, devMethodology, provider;
+	@Column
+	private int idPreUpdate;
+	@ManyToOne(cascade = CascadeType.ALL , fetch = FetchType.EAGER)
+	@JoinColumn(name = "FK_idUtente", referencedColumnName = "idUtente")
+//	@JsonBackReference
+	private Utente utente;
+	@ManyToOne(cascade = CascadeType.ALL , fetch = FetchType.EAGER)
+	@JoinColumn(name = "FK_idApp", referencedColumnName = "idApplicazione")
+//	@JsonBackReference
+	private Applicazione applicazione;
+	public LogFileApp(Integer idLogApp, LocalDateTime data, Integer nodoConsole,
+			Time launchingMeetingDataGatheringStarting, BigDecimal avgAnalysisTime, Date automationEnablingDate,
+			boolean done, String nome_App, String apmCode, String insertedInCastProgram, String stakeholderEngagement,
+			String stakeholderBrief, String onBoardingKitDelivery, String primaRestitution, String ownerOnboarding,
+			String ownerAFP, String gdsUnit, String tecnologia, String serverManager, String soloCMS, String macchina,
+			String noteOnboarding, String fase, String afpStatus, String pubblicatoDashboard, String noteAppOwner,
+			String jiraautomationActivation, String repoAvailability, String automationStatus, String automationNotes,
+			String greenItIndex, String onboardingKitClosing, String sourceCodeFinalDelivery, String linkConfluence,
+			String businessCriticality, String devMethodology, String provider, int idPreUpdate, Utente utente,
+			Applicazione applicazione) {
+		super();
+		this.idLogApp = idLogApp;
+		this.data = data;
+		this.nodoConsole = nodoConsole;
+		this.launchingMeetingDataGatheringStarting = launchingMeetingDataGatheringStarting;
+		this.avgAnalysisTime = avgAnalysisTime;
+		this.automationEnablingDate = automationEnablingDate;
+		this.done = done;
+		this.nome_App = nome_App;
+		this.apmCode = apmCode;
+		this.insertedInCastProgram = insertedInCastProgram;
+		this.stakeholderEngagement = stakeholderEngagement;
+		this.stakeholderBrief = stakeholderBrief;
+		this.onBoardingKitDelivery = onBoardingKitDelivery;
+		this.primaRestitution = primaRestitution;
+		this.ownerOnboarding = ownerOnboarding;
+		this.ownerAFP = ownerAFP;
+		this.gdsUnit = gdsUnit;
+		this.tecnologia = tecnologia;
+		this.serverManager = serverManager;
+		this.soloCMS = soloCMS;
+		this.macchina = macchina;
+		this.noteOnboarding = noteOnboarding;
+		this.fase = fase;
+		this.afpStatus = afpStatus;
+		this.pubblicatoDashboard = pubblicatoDashboard;
+		this.noteAppOwner = noteAppOwner;
+		this.jiraautomationActivation = jiraautomationActivation;
+		this.repoAvailability = repoAvailability;
+		this.automationStatus = automationStatus;
+		this.automationNotes = automationNotes;
+		this.greenItIndex = greenItIndex;
+		this.onboardingKitClosing = onboardingKitClosing;
+		this.sourceCodeFinalDelivery = sourceCodeFinalDelivery;
+		this.linkConfluence = linkConfluence;
+		this.businessCriticality = businessCriticality;
+		this.devMethodology = devMethodology;
+		this.provider = provider;
+		this.idPreUpdate = idPreUpdate;
+		this.utente = utente;
+		this.applicazione = applicazione;
+	}
+	public LogFileApp() {
+		super();
+	}
+	public Integer getIdLogApp() {
+		return idLogApp;
+	}
+	public void setIdLogApp(Integer idLogApp) {
+		this.idLogApp = idLogApp;
+	}
+	public LocalDateTime getData() {
+		return data;
+	}
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	public Integer getNodoConsole() {
+		return nodoConsole;
+	}
+	public void setNodoConsole(Integer nodoConsole) {
+		this.nodoConsole = nodoConsole;
+	}
+	public Time getLaunchingMeetingDataGatheringStarting() {
+		return launchingMeetingDataGatheringStarting;
+	}
+	public void setLaunchingMeetingDataGatheringStarting(Time launchingMeetingDataGatheringStarting) {
+		this.launchingMeetingDataGatheringStarting = launchingMeetingDataGatheringStarting;
+	}
+	public BigDecimal getAvgAnalysisTime() {
+		return avgAnalysisTime;
+	}
+	public void setAvgAnalysisTime(BigDecimal avgAnalysisTime) {
+		this.avgAnalysisTime = avgAnalysisTime;
+	}
+	public Date getAutomationEnablingDate() {
+		return automationEnablingDate;
+	}
+	public void setAutomationEnablingDate(Date automationEnablingDate) {
+		this.automationEnablingDate = automationEnablingDate;
+	}
+	public boolean isDone() {
+		return done;
+	}
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+	public String getNome_App() {
+		return nome_App;
+	}
+	public void setNome_App(String nome_App) {
+		this.nome_App = nome_App;
+	}
+	public String getApmCode() {
+		return apmCode;
+	}
+	public void setApmCode(String apmCode) {
+		this.apmCode = apmCode;
+	}
+	public String getInsertedInCastProgram() {
+		return insertedInCastProgram;
+	}
+	public void setInsertedInCastProgram(String insertedInCastProgram) {
+		this.insertedInCastProgram = insertedInCastProgram;
+	}
+	public String getStakeholderEngagement() {
+		return stakeholderEngagement;
+	}
+	public void setStakeholderEngagement(String stakeholderEngagement) {
+		this.stakeholderEngagement = stakeholderEngagement;
+	}
+	public String getStakeholderBrief() {
+		return stakeholderBrief;
+	}
+	public void setStakeholderBrief(String stakeholderBrief) {
+		this.stakeholderBrief = stakeholderBrief;
+	}
+	public String getOnBoardingKitDelivery() {
+		return onBoardingKitDelivery;
+	}
+	public void setOnBoardingKitDelivery(String onBoardingKitDelivery) {
+		this.onBoardingKitDelivery = onBoardingKitDelivery;
+	}
+	public String getPrimaRestitution() {
+		return primaRestitution;
+	}
+	public void setPrimaRestitution(String primaRestitution) {
+		this.primaRestitution = primaRestitution;
+	}
+	public String getOwnerOnboarding() {
+		return ownerOnboarding;
+	}
+	public void setOwnerOnboarding(String ownerOnboarding) {
+		this.ownerOnboarding = ownerOnboarding;
+	}
+	public String getOwnerAFP() {
+		return ownerAFP;
+	}
+	public void setOwnerAFP(String ownerAFP) {
+		this.ownerAFP = ownerAFP;
+	}
+	public String getGdsUnit() {
+		return gdsUnit;
+	}
+	public void setGdsUnit(String gdsUnit) {
+		this.gdsUnit = gdsUnit;
+	}
+	public String getTecnologia() {
+		return tecnologia;
+	}
+	public void setTecnologia(String tecnologia) {
+		this.tecnologia = tecnologia;
+	}
+	public String getServerManager() {
+		return serverManager;
+	}
+	public void setServerManager(String serverManager) {
+		this.serverManager = serverManager;
+	}
+	public String getSoloCMS() {
+		return soloCMS;
+	}
+	public void setSoloCMS(String soloCMS) {
+		this.soloCMS = soloCMS;
+	}
+	public String getMacchina() {
+		return macchina;
+	}
+	public void setMacchina(String macchina) {
+		this.macchina = macchina;
+	}
+	public String getNoteOnboarding() {
+		return noteOnboarding;
+	}
+	public void setNoteOnboarding(String noteOnboarding) {
+		this.noteOnboarding = noteOnboarding;
+	}
+	public String getFase() {
+		return fase;
+	}
+	public void setFase(String fase) {
+		this.fase = fase;
+	}
+	public String getAfpStatus() {
+		return afpStatus;
+	}
+	public void setAfpStatus(String afpStatus) {
+		this.afpStatus = afpStatus;
+	}
+	public String getPubblicatoDashboard() {
+		return pubblicatoDashboard;
+	}
+	public void setPubblicatoDashboard(String pubblicatoDashboard) {
+		this.pubblicatoDashboard = pubblicatoDashboard;
+	}
+	public String getNoteAppOwner() {
+		return noteAppOwner;
+	}
+	public void setNoteAppOwner(String noteAppOwner) {
+		this.noteAppOwner = noteAppOwner;
+	}
+	public String getJiraautomationActivation() {
+		return jiraautomationActivation;
+	}
+	public void setJiraautomationActivation(String jiraautomationActivation) {
+		this.jiraautomationActivation = jiraautomationActivation;
+	}
+	public String getRepoAvailability() {
+		return repoAvailability;
+	}
+	public void setRepoAvailability(String repoAvailability) {
+		this.repoAvailability = repoAvailability;
+	}
+	public String getAutomationStatus() {
+		return automationStatus;
+	}
+	public void setAutomationStatus(String automationStatus) {
+		this.automationStatus = automationStatus;
+	}
+	public String getAutomationNotes() {
+		return automationNotes;
+	}
+	public void setAutomationNotes(String automationNotes) {
+		this.automationNotes = automationNotes;
+	}
+	public String getGreenItIndex() {
+		return greenItIndex;
+	}
+	public void setGreenItIndex(String greenItIndex) {
+		this.greenItIndex = greenItIndex;
+	}
+	public String getOnboardingKitClosing() {
+		return onboardingKitClosing;
+	}
+	public void setOnboardingKitClosing(String onboardingKitClosing) {
+		this.onboardingKitClosing = onboardingKitClosing;
+	}
+	public String getSourceCodeFinalDelivery() {
+		return sourceCodeFinalDelivery;
+	}
+	public void setSourceCodeFinalDelivery(String sourceCodeFinalDelivery) {
+		this.sourceCodeFinalDelivery = sourceCodeFinalDelivery;
+	}
+	public String getLinkConfluence() {
+		return linkConfluence;
+	}
+	public void setLinkConfluence(String linkConfluence) {
+		this.linkConfluence = linkConfluence;
+	}
+	public String getBusinessCriticality() {
+		return businessCriticality;
+	}
+	public void setBusinessCriticality(String businessCriticality) {
+		this.businessCriticality = businessCriticality;
+	}
+	public String getDevMethodology() {
+		return devMethodology;
+	}
+	public void setDevMethodology(String devMethodology) {
+		this.devMethodology = devMethodology;
+	}
+	public String getProvider() {
+		return provider;
+	}
+	public void setProvider(String provider) {
+		this.provider = provider;
+	}
+	public int getIdPreUpdate() {
+		return idPreUpdate;
+	}
+	public void setIdPreUpdate(int idPreUpdate) {
+		this.idPreUpdate = idPreUpdate;
+	}
+	public Utente getUtente() {
+		return utente;
+	}
+	public void setUtente(Utente utente) {
+		this.utente = utente;
+	}
+	public Applicazione getApplicazione() {
+		return applicazione;
+	}
+	public void setApplicazione(Applicazione applicazione) {
+		this.applicazione = applicazione;
+	}
+	@Override
+	public String toString() {
+		return "LogFileApp [idLogApp=" + idLogApp + ", data=" + data + ", nodoConsole=" + nodoConsole
+				+ ", launchingMeetingDataGatheringStarting=" + launchingMeetingDataGatheringStarting
+				+ ", avgAnalysisTime=" + avgAnalysisTime + ", automationEnablingDate=" + automationEnablingDate
+				+ ", done=" + done + ", nome_App=" + nome_App + ", apmCode=" + apmCode + ", insertedInCastProgram="
+				+ insertedInCastProgram + ", stakeholderEngagement=" + stakeholderEngagement + ", stakeholderBrief="
+				+ stakeholderBrief + ", onBoardingKitDelivery=" + onBoardingKitDelivery + ", primaRestitution="
+				+ primaRestitution + ", ownerOnboarding=" + ownerOnboarding + ", ownerAFP=" + ownerAFP + ", gdsUnit="
+				+ gdsUnit + ", tecnologia=" + tecnologia + ", serverManager=" + serverManager + ", soloCMS=" + soloCMS
+				+ ", macchina=" + macchina + ", noteOnboarding=" + noteOnboarding + ", fase=" + fase + ", afpStatus="
+				+ afpStatus + ", pubblicatoDashboard=" + pubblicatoDashboard + ", noteAppOwner=" + noteAppOwner
+				+ ", jiraautomationActivation=" + jiraautomationActivation + ", repoAvailability=" + repoAvailability
+				+ ", automationStatus=" + automationStatus + ", automationNotes=" + automationNotes + ", greenItIndex="
+				+ greenItIndex + ", onboardingKitClosing=" + onboardingKitClosing + ", sourceCodeFinalDelivery="
+				+ sourceCodeFinalDelivery + ", linkConfluence=" + linkConfluence + ", businessCriticality="
+				+ businessCriticality + ", devMethodology=" + devMethodology + ", provider=" + provider
+				+ ", idPreUpdate=" + idPreUpdate + ", utente=" + utente + ", applicazione=" + applicazione + "]";
+	}
+	
+	
+}
---
+++ src/main/java/it/progettogestionale/web/model/LogFileRescan.java
@@ -0,0 +1,176 @@
+package it.progettogestionale.web.model;
+
+import java.sql.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "logfilerescan")
+public class LogFileRescan {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idLogRescan;
+	@Column(name="data")
+	private Date data;
+	@Column
+	private int nRescan;
+	@Column
+	private boolean ongoing;
+	@Column
+	private boolean archive;
+	@Column
+	private Integer newOb;
+	@Column
+	private Integer py;
+	@Column
+	private Integer ytd;
+	@Column
+	private String afpe;
+	@Column
+	private String yoyRolling;
+	@Column
+	private String last_Rescan;
+	@Column
+	private Date rkd;
+	@Column
+	private int idPreUpdate;
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "FK_idUtente", referencedColumnName = "idUtente")
+//	@JsonBackReference
+	private Utente utente;
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "FK_idRescan", referencedColumnName = "idRescan")
+//	@JsonBackReference
+	private Rescan rescan;
+	public LogFileRescan(Integer idLogRescan, Date data, int nRescan, boolean ongoing, boolean archive, Integer newOb,
+			Integer py, Integer ytd, String afpe, String yoyRolling, String last_Rescan, Date rkd, int idPreUpdate,
+			Utente utente, Rescan rescan) {
+		super();
+		this.idLogRescan = idLogRescan;
+		this.data = data;
+		this.nRescan = nRescan;
+		this.ongoing = ongoing;
+		this.archive = archive;
+		this.newOb = newOb;
+		this.py = py;
+		this.ytd = ytd;
+		this.afpe = afpe;
+		this.yoyRolling = yoyRolling;
+		this.last_Rescan = last_Rescan;
+		this.rkd = rkd;
+		this.idPreUpdate = idPreUpdate;
+		this.utente = utente;
+		this.rescan = rescan;
+	}
+	public LogFileRescan() {
+		super();
+	}
+	public Integer getIdLogRescan() {
+		return idLogRescan;
+	}
+	public void setIdLogRescan(Integer idLogRescan) {
+		this.idLogRescan = idLogRescan;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public int getnRescan() {
+		return nRescan;
+	}
+	public void setnRescan(int nRescan) {
+		this.nRescan = nRescan;
+	}
+	public boolean isOngoing() {
+		return ongoing;
+	}
+	public void setOngoing(boolean ongoing) {
+		this.ongoing = ongoing;
+	}
+	public boolean isArchive() {
+		return archive;
+	}
+	public void setArchive(boolean archive) {
+		this.archive = archive;
+	}
+	public Integer getNewOb() {
+		return newOb;
+	}
+	public void setNewOb(Integer newOb) {
+		this.newOb = newOb;
+	}
+	public Integer getPy() {
+		return py;
+	}
+	public void setPy(Integer py) {
+		this.py = py;
+	}
+	public Integer getYtd() {
+		return ytd;
+	}
+	public void setYtd(Integer ytd) {
+		this.ytd = ytd;
+	}
+	public String getAfpe() {
+		return afpe;
+	}
+	public void setAfpe(String afpe) {
+		this.afpe = afpe;
+	}
+	public String getYoyRolling() {
+		return yoyRolling;
+	}
+	public void setYoyRolling(String yoyRolling) {
+		this.yoyRolling = yoyRolling;
+	}
+	public String getLast_Rescan() {
+		return last_Rescan;
+	}
+	public void setLast_Rescan(String last_Rescan) {
+		this.last_Rescan = last_Rescan;
+	}
+	public Date getRkd() {
+		return rkd;
+	}
+	public void setRkd(Date rkd) {
+		this.rkd = rkd;
+	}
+	public int getIdPreUpdate() {
+		return idPreUpdate;
+	}
+	public void setIdPreUpdate(int idPreUpdate) {
+		this.idPreUpdate = idPreUpdate;
+	}
+	public Utente getUtente() {
+		return utente;
+	}
+	public void setUtente(Utente utente) {
+		this.utente = utente;
+	}
+	public Rescan getRescan() {
+		return rescan;
+	}
+	public void setRescan(Rescan rescan) {
+		this.rescan = rescan;
+	}
+	@Override
+	public String toString() {
+		return "LogFileRescan [idLogRescan=" + idLogRescan + ", data=" + data + ", nRescan=" + nRescan + ", ongoing="
+				+ ongoing + ", archive=" + archive + ", newOb=" + newOb + ", py=" + py + ", ytd=" + ytd + ", afpe="
+				+ afpe + ", yoyRolling=" + yoyRolling + ", last_Rescan=" + last_Rescan + ", rkd=" + rkd
+				+ ", idPreUpdate=" + idPreUpdate + ", utente=" + utente + ", rescan=" + rescan + "]";
+	}
+	
+	
+}
