5720ba37e76804325e624e213851c0fcbf6b0179
fra-nce-sco <1francesconappi@gmail.com>
2022-07-08 12:33:00 +0200

    modifica login

--- src/main/java/it/progettogestionale/web/rest/UtenteRest.java
+++ src/main/java/it/progettogestionale/web/rest/UtenteRest.java
@@ -99,6 +99,7 @@ public class UtenteRest {
 		Utente alfonso = utenteRepo.findByEmail(u.getEmail());
 		try {
 			if( alfonso.getEmail().equals(u.getEmail()) && alfonso.getPassword().equals(u.getPassword()) ) {
+				u = alfonso;
 				u.setAccesso(true);
 				return ResponseEntity.ok(u);
 			}
