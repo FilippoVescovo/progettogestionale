e27493335fc36940cf6dab1b48048434f12a61ea
fra-nce-sco <1francesconappi@gmail.com>
2022-07-08 13:39:38 +0200

    Aggiunta servizio
    - servizio logout

--- src/main/java/it/progettogestionale/web/rest/UtenteRest.java
+++ src/main/java/it/progettogestionale/web/rest/UtenteRest.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -106,5 +107,12 @@ public class UtenteRest {
 		}catch(NullPointerException e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
+	}
+	
+	@PutMapping("/logout")
+	public void logout(@RequestBody Utente u) {
+		Utente alfonso = utenteRepo.findById(u.getIdUtente()).get();
+		alfonso.setAccesso(false);
+		utenteRepo.save(alfonso);
 	}
 }
